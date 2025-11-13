bffa4d5ea9529b015583a79747b54565531919b9
Winston Tsia <winstont@mhconsults.com>
2025-11-13 08:36:31 -0500

    updating script

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 
 
 Run the following to execute locally:
+
 `Set-ExecutionPolicy RemoteSigned -Scope Process -Force`
+
+Then:
+
+`iex (Invoke-RestMethod -Uri "https://raw.githubusercontent.com/mhwint/scripts/refs/heads/main/mwbRun.ps1")`
 
--- mwbRun.ps1
+++ mwbRun.ps1
@@ -1,7 +1,7 @@
 Clear-Host
 
 $MB_Service = "C:\Program Files\Malwarebytes\Anti-Malware\MBAMService.exe"
-$WaitTimeSeconds = 300
+$WaitTimeSeconds = 180
 
 Write-Host "Starting MWB Update"
 Start-Process -FilePath $MB_Service -ArgumentList "-update" -ErrorAction Stop
@@ -12,4 +12,5 @@ Start-Sleep -Seconds $WaitTimeSeconds
 Write-Host "Scanning"
 Start-Process -FilePath $MB_Service -ArgumentList "-scan -threat" -ErrorAction Stop
 
-Remove-Item $MyInvocation.MyCommand.Path -ErrorAction SilentlyContinue
+# Self Deletion
+# Remove-Item $MyInvocation.MyCommand.Path -ErrorAction SilentlyContinue
